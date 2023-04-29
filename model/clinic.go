22d9b637ce5f1c35a5838fcefa5c08a362b5fddd
MakotoNakai <macofandohr1812@gmail.com>
2023-04-29 16:39:22 +0900

    [feat] add models

--- model/clinic.go
+++ model/clinic.go
@@ -1,12 +1,41 @@
 package model
 
 import (
-	"time"
+	"fmt"
+	"errors"
+	"gorm.io/gorm"
 )
 
 type Clinic struct {
-	ID int
-	Name string 
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID int `json:"id" param:"id" gorm:"primaryKey;autoIncrement"`
+	Name string `json:"name"`
+}
+
+func (c *Clinic) Create() {
+	db.Create(&c)
+}
+
+func (c *Clinic) GetAll() []Clinic {
+	clinics := []Clinic{}
+	err := db.Find(&clinics).Error
+	if err != nil {
+		fmt.Println("Error found")
+	}
+	return clinics
+}
+ 
+func (c *Clinic) GetById(id int) {
+	err := db.Table("clinics").Find(&c,"id = ?", id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+			fmt.Println("No user found")
+	}
+}
+
+func (c *Clinic) Update() {
+	db.Save(&c)
+}
+
+func (c *Clinic) Delete() {
+	c_ := Clinic{}
+  db.Delete(&c_, "id = ?", c.ID)
 }
--- model/user.go
+++ model/user.go
@@ -1,12 +1,44 @@
 package model
 
 import (
-	"time"
+	"fmt"
+	"errors"
+	"gorm.io/gorm"
+	"github.com/makotonakai/medical-hackathon/database"
 )
 
-type Model struct {
-	ID int 
-	Name string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+var db = database.Connect()
+
+type User struct {
+	ID int `json:"id" param:"id" gorm:"primaryKey;autoIncrement"`
+	Name string `json:"name" param:"name"`
+}
+
+func (u *User) Create() {
+	db.Create(&u)
+}
+
+func (u *User) GetAll() []User {
+	users := []User{}
+	err := db.Find(&users).Error
+	if err != nil {
+		fmt.Println("Error found")
+	}
+	return users
+}
+ 
+func (u *User) GetById(id int) {
+	err := db.Table("users").Find(&u,"id = ?", id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+			fmt.Println("No user found")
+	}
+}
+
+func (u *User) Update() {
+	db.Save(&u)
+}
+
+func (u *User) Delete() {
+	u_ := User{}
+  db.Delete(&u_, "id = ?", u.ID)
 }
