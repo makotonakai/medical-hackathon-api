6e1bc5257219c4b008bf93ff743b64f9b9e9709c
MakotoNakai <macofandohr1812@gmail.com>
2023-04-29 16:39:43 +0900

    [feat] add controllers

---
+++ controller/clinic.go
@@ -0,0 +1,79 @@
+package controller
+
+import (
+	"fmt"
+	"strconv"
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/makotonakai/medical-hackathon/model"
+)
+
+//----------
+// Handlers
+//----------
+
+func CreateClinic(c echo.Context) error {
+	
+	clinic := model.Clinic{}
+	
+	err := c.Bind(&clinic)
+	if err != nil {
+		fmt.Println("Failed to create Clinic")
+	}
+
+	clinic.Create()
+	return c.JSON(http.StatusCreated, clinic)
+	
+}
+
+func GetClinics(c echo.Context) error {
+
+	clinic := model.Clinic{}
+	clinicList := clinic.GetAll()
+	return c.JSON(http.StatusOK, clinicList)
+
+}
+
+func GetClinicById(c echo.Context) error {
+
+	clinic := model.Clinic{}
+	err := c.Bind(&clinic)
+
+	id_ := c.Param("id")
+	id, err := strconv.Atoi(id_)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	clinic.GetById(id)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, clinic)
+}
+
+func UpdateClinic(c echo.Context) error {
+
+	clinic := model.Clinic{}
+	err := c.Bind(&clinic)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	clinic.Update()
+	return c.JSON(http.StatusOK, clinic)
+}
+
+func DeleteClinic(c echo.Context) error {
+
+	clinic := model.Clinic{}
+	
+	err := c.Bind(&clinic)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	clinic.Delete()
+	return c.JSON(http.StatusNoContent, clinic)
+}
---
+++ controller/user.go
@@ -0,0 +1,79 @@
+package controller
+
+import (
+	"fmt"
+	"strconv"
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/makotonakai/medical-hackathon/model"
+)
+
+//----------
+// Handlers
+//----------
+
+func CreateUser(c echo.Context) error {
+	
+	user := model.User{}
+	
+	err := c.Bind(&user)
+	if err != nil {
+		fmt.Println("Failed to create user")
+	}
+
+	user.Create()
+	return c.JSON(http.StatusCreated, user)
+	
+}
+
+func GetUsers(c echo.Context) error {
+
+	u := model.User{}
+	userList := u.GetAll()
+	return c.JSON(http.StatusOK, userList)
+
+}
+
+func GetUserById(c echo.Context) error {
+
+	user := model.User{}
+	err := c.Bind(&user)
+
+	id_ := c.Param("id")
+	id, err := strconv.Atoi(id_)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	user.GetById(id)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func UpdateUser(c echo.Context) error {
+
+	user := model.User{}
+	err := c.Bind(&user)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	user.Update()
+	return c.JSON(http.StatusOK, user)
+}
+
+func DeleteUser(c echo.Context) error {
+
+	user := model.User{}
+	
+	err := c.Bind(&user)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	user.Delete()
+	return c.JSON(http.StatusNoContent, user)
+}
