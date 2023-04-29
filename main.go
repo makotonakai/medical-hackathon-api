4bb685639ecbc294ff4eb30cb9eb3bdcc8a48429
MakotoNakai <macofandohr1812@gmail.com>
2023-04-29 16:40:21 +0900

    [feat] add main.go

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"net/http"
-	
-	"github.com/labstack/echo/v4"
+	"github.com/makotonakai/medical-hackathon/router"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e := router.Initialize()
 	e.Logger.Fatal(e.Start(":1323"))
 }
