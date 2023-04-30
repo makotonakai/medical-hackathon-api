798553e1de50ae488273fe5cf88bc4e0b65dd5f4
MakotoNakai <macofandohr1812@gmail.com>
2023-04-30 14:29:01 +0900

    [fix] fix typo

--- app/database/sql/family_history.sql
+++ app/database/sql/family_history.sql
@@ -1,8 +1,8 @@
 ---- drop ----
-DROP TABLE IF EXISTS family_histroies;
+DROP TABLE IF EXISTS family_histories;
 
 ---- create ----
-create table IF not exists family_histroies
+create table IF not exists family_histories
 (
  `id`               INT(20) AUTO_INCREMENT,
  `user_id`          INT(20) NOT NULL,
@@ -12,4 +12,4 @@ create table IF not exists family_histroies
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-insert into family_histroies values (1, 1, "父が頭痛持ちです", current_timestamp, current_timestamp);
+insert into family_histories values (1, 1, "父が頭痛持ちです", current_timestamp, current_timestamp);
